7ca790bd6901e314fb306cf8356d26cbebf9d0c4
Jasmir Medina <201848984+fn-cafeina@users.noreply.github.com>
2026-03-01 16:07:15 -0600

    Change primeNumber to a real prime number

--- hashmap.js
+++ hashmap.js
@@ -7,7 +7,7 @@ export default class HashMap {
   hash(key) {
     let hashCode = 0;
 
-    const primeNumber = 32;
+    const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
       hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
     }
