7dd8dc3cbe4604f1c1710ddd9904becc0451fef1
Jasmir Medina <201848984+fn-cafeina@users.noreply.github.com>
2026-03-01 20:50:02 -0600

    Add LinkedList

---
+++ linked-list.js
@@ -0,0 +1,24 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.nextNode = null;
+  }
+}
+
+export default class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(value) {
+    const node = new Node(value);
+
+    if (!this.head) {
+      this.head = node;
+    } else {
+      const pointer = this.head;
+      while (pointer.nextNode) pointer = pointer.nextNode;
+      pointer.nextNode = node;
+    }
+  }
+}
