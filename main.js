f8001ca4e5a87d5f47be465504147d768a862f32
Jasmir Medina <201848984+fn-cafeina@users.noreply.github.com>
2026-03-03 00:49:52 -0600

    Add tests

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
 import HashMap from "./hashmap.js";
 
 const test = new HashMap();
+
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+test.set("kite", "pink");
+test.set("lion", "golden");
+
+console.log(test.buckets);
+
+test.set("moon", "silver");
+
+console.log(test.buckets);
+
+console.log(test.get("banana"));
+console.log(test.has("apple"));
+console.log(test.remove("frog"));
+console.log(test.length());
+console.log(test.keys());
+console.log(test.values());
+console.log(test.entries());
+test.clear();
+
+console.log(test.buckets);
